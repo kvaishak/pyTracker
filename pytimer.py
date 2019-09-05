315bef9a1778254a703d351b2ef74fa68bb1fa18
vaishak.k <vaishak.k@zohocorp.com>
2019-09-05 11:19:41 +0530

    Added code for monitoring the websites within chrome.

--- pytimer.py
+++ pytimer.py
@@ -1,21 +1,37 @@
 import time
 import datetime
 from AppKit import NSWorkspace
+from Foundation import *
 
 active_window_name = ""
 activity_name = ""
 first_time = True
 start_time = datetime.datetime.now()
 
+def url_to_name(url):
+    string_list = url.split('/')
+    return string_list[2]
+
 def get_active_window():    
     _active_window_name = (NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName'])
     return _active_window_name
+
+def get_chrome_url():
+    textOfMyScript = """tell app "google chrome" to get the url of the active tab of window 1"""
+    s = NSAppleScript.initWithSource_(
+        NSAppleScript.alloc(), textOfMyScript)
+    results, err = s.executeAndReturnError_(None)
+    return results.stringValue()
 
 try:
     while True:
         previous_site = ""
         
         new_window_name = get_active_window()
+        # for getting the chrome Url
+        if 'Google Chrome' in new_window_name:
+                new_window_name = url_to_name(get_chrome_url())
+
         if active_window_name != new_window_name:
             print(active_window_name)
             activity_name = active_window_name
